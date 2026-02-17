8c07e168afde187f00e7072c64e90cb7cb4c2c5f
yoshimasa0610 <higuti.yoshimasa@gmail.com>
2026-02-17 14:29:56 +0900

    tintin

--- SADNESS KNIGHT/Source/Map/Checkpoint/CheckpointManager.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// 椅子（チェックポイント）に座っているかどうかを管理するシンプルな API
-// 他モジュールからは IsPlayerSitting() を呼び、Checkpoint が座らせるときは SetPlayerSitting(true) を呼ぶ。
-
-void SetPlayerSitting(bool sitting);
-bool IsPlayerSitting();
