a8b4f88e4482795f66bbd75d3f89e17be17d6f56
松元遥斗 <ooi2572051@stu.o-hara.ac.jp>
2026-02-20 15:11:30 +0900

    スキルいじり倒しました

--- SADNESS KNIGHT/Source/Skill/SkillManager.h
+++ SADNESS KNIGHT/Source/Skill/SkillManager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <memory>
+#include <unordered_map>
 #include "Skill.h"
 
 class SkillManager
@@ -8,19 +9,24 @@ class SkillManager
 private:
     std::vector<std::shared_ptr<Skill>> m_ownedSkills;
 
+    // スキルID → 残り使用回数
+    std::unordered_map<int, int> m_remainingUses;
+
     int m_equipSlots[2][3];
     int m_currentSet;
 
 public:
     SkillManager();
 
-    void AddSkill(const SkillData& data);
+    void AddSkill(const SkillData& data, PlayerData* player);
     void EquipSkill(int setIndex, int slotIndex, int skillID);
 
     void ChangeSet();
     void UseSkill(int slotIndex, PlayerData* player);
 
     void Update(PlayerData* player);
+    // 再計算
+    void RecalculateUses(PlayerData* player);
 
     const std::vector<std::shared_ptr<Skill>>& GetSkills() const
     {
